5a7cb254f11625e6616b77afd040c2399351b4b3
ALDI-JAYA <91648104+ALDI-JAYA@users.noreply.github.com>
2022-02-01 09:53:23 +0700

    Tutle

---
+++ TURTLE/sprilar.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.bgcolor("Black")
+Cppsecrets = turtle.Screen()
+Cppsecrets.title("Lingkaran ")
+turtle=turtle.Turtle()
+turtle.color("White")
+turtle.speed(0)
+turtle.hideturtle()
+for i in range(150):
+    turtle.circle(i*1)
+    turtle._rotate(5)
+
